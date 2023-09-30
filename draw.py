cc5dfae521f065b6f1290e15543e127b23649744
IsrakAhmed <israkahmed7@gmail.com>
2023-10-01 00:58:20 +0600

    Drawing Rectangle

--- draw.py
+++ draw.py
@@ -6,7 +6,16 @@ cv2.imshow('Blank',blank)
 
 # 1. Paint the image a certain colour
 
-blank[200:300, 300:400] = 0,0,255
-cv2.imshow('Red',blank)
+# blank[200:300, 300:400] = 0,0,255
+# cv2.imshow('Red',blank)
+
+
+
+# 2. Draw a Rectangle
+
+cv2.rectangle(blank, (0,0), (250,250), (0,255,0), thickness=2)
+cv2.imshow('Rectangle', blank)
+
+
 
 cv2.waitKey(0)
