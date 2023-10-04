9554d430b081cc38011b2685a536fc6eca90d558
IsrakAhmed <israkahmed7@gmail.com>
2023-10-04 12:10:03 +0600

    Writing Text

--- draw.py
+++ draw.py
@@ -14,19 +14,28 @@ cv2.imshow('Blank',blank)
 # 2. Draw a Rectangle
 
 cv2.rectangle(blank, (0,0), (blank.shape[1]//2,blank.shape[0]//2), (0,255,0), thickness=cv2.FILLED)
-cv2.imshow('Rectangle', blank)
+# cv2.imshow('Rectangle', blank)
 
 
 
 # 3. Draw a Circle
 
 cv2.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0,0,255), thickness=2)
-cv2.imshow('Circle', blank)
+# cv2.imshow('Circle', blank)
 
 
-# Draw a Line
+# 4. Draw a Line
 
 cv2.line(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (255,255,255), thickness=3)
-cv2.imshow("Line",blank)
+# cv2.imshow("Line",blank)
+
+
+
+# 5. Write Text
+
+cv2.putText(blank, 'Hello There', (180,400), cv2.FONT_HERSHEY_TRIPLEX, 1.0,(0,255,0), 2)
+cv2.imshow("Text",blank)
+
+
 
 cv2.waitKey(0)
